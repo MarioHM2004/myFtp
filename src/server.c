feaacc62f0f0214eeffc533dcfded820e5f320be
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-04 17:17:47 +0100

    [feat]: added loop and fork to manage several clients at the same time without a queue

--- src/server.c
+++ src/server.c
@@ -10,11 +10,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
 int main(int ac, char **av)
 {
-    char server_message[256] = "You have reached the server!";
+    if (ac < 2) {
+    fprintf(stderr, "Usage: %s <port>\n", av[0]);
+    return 1;
+    }
+
+    char server_message[256] = "Hello World!!!\n";
 
     int server_socket, client_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -24,14 +30,48 @@ int main(int ac, char **av)
     server_addr.sin_port = htons(atoi(av[1]));
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (server_socket == -1) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
+
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
+        perror("Binding failed");
+        return 1;
+    }
 
     listen(server_socket, 5);
 
-    client_socket = accept(server_socket, NULL, NULL);
-    send(client_socket, server_message, sizeof(server_message), 0);
+    while (1) {
+        client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket == -1) {
+            perror("Accept failed");
+            return 1;
+        }
 
-    close(server_socket);
+        pid_t p = fork();
+        if (p < 0) {
+            perror("fork fail");
+            exit(1);
+        } else if (p == 0) {
+            close(server_socket);
+            sleep(5);
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            if (getpeername(client_socket, (struct sockaddr*)&client_addr, &client_addr_len) == -1) {
+                perror("getpeername failed");
+                return 1;
+            }
+            printf("Connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+            send(client_socket, server_message, sizeof(server_message), 0);
+            close(client_socket);
+            exit(0); 
+        } else {
+            close(client_socket);             
+        }
+    }
 
     return 0;
 }
