6c306922da5b6ce6e391e1402c35d9a893b38e14
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-11 16:07:51 +0100

    [feat]: project prototypes

---
+++ include/include.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** include.h
+** File description:
+** header files
+*/
+
+#ifndef SERVER_H
+    #define SERVER_H
+
+void usage_help(int ac, char **av);
+void error(const char *msg);
+
+#endif
