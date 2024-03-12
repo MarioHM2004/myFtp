3a6b9d9878504d84b3bdf2817c35f1ca4403b48a
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-12 17:56:57 +0100

    [feat]: function for sending error in wrong command case and function to send a message to the client

--- src/help.c
+++ src/help.c
@@ -8,12 +8,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "../include/include.h"
 
 void error(const char *msg)
 {
     printf("%s\n", msg);
     exit(EXIT_FAILURE);
+}
+
+void command_error(int server_response)
+{
+    char *msg = "xxx Error (RFC compliant)";
+    char *error_message = malloc(sizeof(char)*strlen(msg)+1);
+    strcpy(error_message, msg);
+    if (write(server_response, &error_message, strlen(error_message)) == -1) {
+        error("write failed");
+    }
+    free(error_message);
+}
+
+void send_msg_to_client(int server_response, char *msg)
+{
+    char *server_message = malloc(sizeof(char)*strlen(msg)+1);
+    strcpy(server_message, msg);
+    if (write(server_response, &server_message, strlen(server_message)) == -1) {
+        error("write failed");
+    }
+    printf("%s", server_message);
+    free(server_message);
 }
 
 void usage_help(int ac, char **av)
@@ -24,8 +47,8 @@ void usage_help(int ac, char **av)
     <path>  is the path to the home directory for the Anonymous user\n");
         exit(EXIT_SUCCESS);
     }
-    else if (ac != 3) {
+    /*else if (ac != 3) {
         error("usage: ./myftp <port> <path>\n\
     for more info: ./myftp -help");
-    }
+    }*/
 }
