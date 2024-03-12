f9cac0fab65726bdb334f51b754d08a89374c54b
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-12 17:55:57 +0100

    [feat]: added parsing of client commands, half implemented USER command

--- src/server/server.c
+++ src/server/server.c
@@ -32,32 +32,49 @@ int create_server_socket(int port)
     return server_socket;
 }
 
+void client_commands(int client_socket, char *client_response)
+{
+    char *command = strtok(client_response, " ");
+    char **command_arr = malloc(sizeof(char*)*2);
+    for (int i = 0; command != NULL; i++) {
+        if (i >=3)
+            command_error(client_socket);
+        command_arr[i] = command;
+        command = strtok(NULL, " ");
+    }
+    if (strcmp(command_arr[0], "USER") == 0 && strcmp(command_arr[1], "Anonymous") == 0) {
+        send_msg_to_client(client_socket, "User name okay, need password.");
+    } else {
+        command_error(client_socket);
+    }
+
+}
+
 void client_connection(int client_socket)
 {
-    char *message = "Hello World!!!\n";
-    char *server_message = malloc(sizeof(char)*strlen(message) + 1);
-    strcpy(server_message, message);
-    send(client_socket, server_message, strlen(server_message)+1, 0);
+    char *client_response = malloc(sizeof(char)*500);
+    if (read(client_socket, client_response, 500) == -1)
+        error("read failed");
+    client_commands(client_socket, client_response);
     close(client_socket);
-    free(server_message);
+    free(client_response);
 }
 
 void run_server(int server_socket)
 {
+    int client_socket = 0;
     fd_set current_sockets, ready_sockets;
     FD_ZERO(&current_sockets);
     FD_SET(server_socket, &current_sockets);
 
     while (1) {
         ready_sockets = current_sockets;
-
         if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0) 
             error("select error");
-
         for (int i = 0; i < FD_SETSIZE; i++) {
             if (FD_ISSET(i, &ready_sockets)) {
                 if (i == server_socket) {
-                    int client_socket = accept(server_socket, NULL, NULL);
+                    client_socket = accept(server_socket, NULL, NULL);
                     printf("client connected\n");
                     FD_SET(client_socket, &current_sockets);
                 } else {
@@ -71,8 +88,10 @@ void run_server(int server_socket)
 
 int main(int ac, char **av)
 {
+    int server_socket = 0;
+
     usage_help(ac, av);
-    int server_socket = create_server_socket(atoi(av[1]));
+    server_socket = create_server_socket(atoi(av[1]));
     run_server(server_socket);
     close(server_socket);
     return 0;
