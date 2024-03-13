3097002e90b6b78392a7bcf7f8901cf430e58791
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-13 16:59:22 +0100

    [fix]: user login, coding style

--- src/server/server.c
+++ src/server/server.c
@@ -32,19 +32,25 @@ int create_server_socket(int port)
     return server_socket;
 }
 
+void help_connection(int i, int server_socket,
+    int client_socket, fd_set *current_sockets)
+{
+    if (i == server_socket) {
+        client_socket = accept(server_socket, NULL, NULL);
+        printf("client connected\n");
+        FD_SET(client_socket, current_sockets);
+    } else {
+        client_connection(i);
+        FD_CLR(i, current_sockets);
+    }
+}
+
 void connection_manager(int server_socket, int client_socket,
-    fd_set current_sockets, fd_set ready_sockets)
+    fd_set *current_sockets, fd_set *ready_sockets)
 {
     for (int i = 0; i < FD_SETSIZE; i++) {
-        if (FD_ISSET(i, &ready_sockets)) {
-            if (i == server_socket) {
-                client_socket = accept(server_socket, NULL, NULL);
-                printf("client connected\n");
-                FD_SET(client_socket, &current_sockets);
-            } else {
-                client_connection(i);
-                FD_CLR(i, &current_sockets);
-            }
+        if (FD_ISSET(i, ready_sockets)) {
+            help_connection(i, server_socket, client_socket, current_sockets);
         }
     }
 }
@@ -52,15 +58,17 @@ void connection_manager(int server_socket, int client_socket,
 void run_server(int server_socket)
 {
     int client_socket = 0;
+    fd_set current_sockets;
+    fd_set ready_sockets;
 
-    fd_set current_sockets, ready_sockets;
     FD_ZERO(&current_sockets);
     FD_SET(server_socket, &current_sockets);
-
     while (1) {
         ready_sockets = current_sockets;
-        if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0) 
+        if (select(FD_SETSIZE, &ready_sockets,
+            NULL, NULL, NULL) < 0)
             error("select error");
-        connection_manager(server_socket, client_socket, current_sockets, ready_sockets);
+        connection_manager(server_socket, client_socket,
+            &current_sockets, &ready_sockets);
     }
 }
