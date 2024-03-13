2df65e16c273e029beef03cbef8130c8b9d129f1
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-03-13 12:56:28 +0100

    [refactor]: uncommented condition and changed wrong name of variables

--- src/help.c
+++ src/help.c
@@ -17,25 +17,24 @@ void error(const char *msg)
     exit(EXIT_FAILURE);
 }
 
-void command_error(int server_response)
+void command_error(int server_socket)
 {
-    char *msg = "xxx Error (RFC compliant)";
+    char *msg = "xxx Error (RFC compliant)\n";
     char *error_message = malloc(sizeof(char)*strlen(msg)+1);
     strcpy(error_message, msg);
-    if (write(server_response, &error_message, strlen(error_message)) == -1) {
+    if (write(server_socket, error_message, strlen(error_message)) == -1) {
         error("write failed");
     }
     free(error_message);
 }
 
-void send_msg_to_client(int server_response, char *msg)
+void send_msg_to_client(int server_socket, char *msg)
 {
     char *server_message = malloc(sizeof(char)*strlen(msg)+1);
     strcpy(server_message, msg);
-    if (write(server_response, &server_message, strlen(server_message)) == -1) {
+    if (write(server_socket, server_message, strlen(server_message)) == -1) {
         error("write failed");
     }
-    printf("%s", server_message);
     free(server_message);
 }
 
@@ -47,8 +46,8 @@ void usage_help(int ac, char **av)
     <path>  is the path to the home directory for the Anonymous user\n");
         exit(EXIT_SUCCESS);
     }
-    /*else if (ac != 3) {
+    else if (ac != 3) {
         error("usage: ./myftp <port> <path>\n\
     for more info: ./myftp -help");
-    }*/
+    }
 }
