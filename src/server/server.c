e70acd562c8cc882c91edb14aedb09a6b4955369
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-05-09 10:27:12 +0200

    [refacto]: updated code with server struct

--- src/server/server.c
+++ src/server/server.c
@@ -11,64 +11,59 @@
 #include <sys/select.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include "../../include/include.h"
+#include "../../include/server.h"
 
-int create_server_socket(int port)
+int create_server_socket(server_s *server, int port)
 {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server->server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
 
-    if (server_socket == -1)
+    if (server->server_socket == -1)
         error("Socket creation failed");
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_socket, (struct sockaddr *)&server_addr,
+    if (bind(server->server_socket, (struct sockaddr *)&server_addr,
         sizeof(server_addr)) == -1) {
         error("Binding failed");
     }
     printf("waiting for client connections...\n");
-    listen(server_socket, 5);
-    return server_socket;
+    listen(server->server_socket, 5);
+    return server->server_socket;
 }
 
-void help_connection(int i, int server_socket,
-    int client_socket, fd_set *current_sockets)
+void help_connection(server_s *server)
 {
-    if (i == server_socket) {
-        client_socket = accept(server_socket, NULL, NULL);
+    if (server->i == server->server_socket) {
+        server->client_socket = accept(server->server_socket, NULL, NULL);
         printf("client connected\n");
-        FD_SET(client_socket, current_sockets);
+        FD_SET(server->client_socket, &(server->current_sockets));
     } else {
-        client_connection(i);
-        FD_CLR(i, current_sockets);
+        client_connection(server);
+        FD_CLR(server->i, &(server->current_sockets));
     }
 }
 
-void connection_manager(int server_socket, int client_socket,
-    fd_set *current_sockets, fd_set *ready_sockets)
+void connection_manager(server_s *server)
 {
-    for (int i = 0; i < FD_SETSIZE; i++) {
-        if (FD_ISSET(i, ready_sockets)) {
-            help_connection(i, server_socket, client_socket, current_sockets);
+    for (server->i = 0; server->i < FD_SETSIZE; server->i++) {
+        if (FD_ISSET(server->i, &server->ready_sockets)) {
+            help_connection(server);
         }
     }
 }
 
-void run_server(int server_socket)
+void run_server(server_s *server)
 {
-    int client_socket = 0;
-    fd_set current_sockets;
-    fd_set ready_sockets;
+    server->client_socket = 0;
 
-    FD_ZERO(&current_sockets);
-    FD_SET(server_socket, &current_sockets);
+    FD_ZERO(&server->current_sockets);
+    FD_SET(server->server_socket, &server->current_sockets);
     while (1) {
-        ready_sockets = current_sockets;
-        if (select(FD_SETSIZE, &ready_sockets,
+        server->ready_sockets = server->current_sockets;
+        if (select(FD_SETSIZE, &server->ready_sockets,
             NULL, NULL, NULL) < 0)
             error("select error");
-        connection_manager(server_socket, client_socket,
-            &current_sockets, &ready_sockets);
+        connection_manager(server);
     }
 }
