53d8814e4637d7c962aff842065c91b3b9a8c745
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-05-13 17:07:29 +0200

    [feat]: added function type and struct for commands handling

--- include/server.h
+++ include/server.h
@@ -9,6 +9,7 @@
     #define SERVER_H
 
 #include <sys/select.h>
+#define MAX_BYTES 1024
 
 typedef struct server_s {
     int server_socket;
@@ -20,12 +21,23 @@ typedef struct server_s {
     int i;
 } server_s;
 
+typedef void (*cmd_fn_t)(server_s *, char **args);
+
+typedef struct {
+    const char *cmd;
+    cmd_fn_t fn;
+} cmd_t;
+
 void usage_help(int ac, char **av);
 void error(const char *msg);
-void command_error(server_s *server);
+void command_error(server_s *server, char *msg);
 void send_msg_to_client(server_s *server, char *msg);
 void client_connection(server_s *server);
 void run_server(server_s *server);
 int create_server_socket(server_s *server, int port);
+void client_commands(server_s *server);
+void command_parsing(server_s *server);
+void cmd_quit(server_s *server, char **args);
+void cmd_user(server_s *server, char **args);
 
 #endif
