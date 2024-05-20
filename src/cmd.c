d32db26d273720275fe69abdeea8600be9335160
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-05-20 17:15:20 +0200

    [feat]: implemented USER, PASS and QUIT cmds logic

--- src/cmd.c
+++ src/cmd.c
@@ -10,28 +10,60 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 #include "../include/server.h"
 
-void cmd_user(server_s *server, char **args)
+void cmd_user(server_t *server, char **args)
 {
+    enum ErrorKinds error = ALL_GOOD;
+    size_t length = 0;
     char *username = args[1];
 
-    if (username == NULL) {
-        return command_error(server, "501");
+    for (int i = 0; args[i] != NULL; i++)
+        length++;
+    user_t *user = server->users;
+    bool is_logged = (user->logged_in & server->client_socket) == server->client_socket;
+    if (length != 2 || username == NULL)
+        return send_msg_to_client(server, message(INVALID_ARGUMENTS));
+    if (is_logged && (strcmp(user->username, username) == 0))
+        return send_msg_to_client(server, message(USER_LOGGED));
+    if (strcmp(user->username, username) != 0)
+        return send_msg_to_client(server, message(INVALID_USERNAME));
+    if (strcmp(user->username, username) == 0) {
+        user->awaiting_pass = server->client_socket;
+        return send_msg_to_client(server, "331 User name okay, need password.");
     }
-    if (strcmp(username, "Anonymous") == 0) {
-        return send_msg_to_client(server, "331 User name okay, need password.\n");
-    }
-    return command_error(server, "530");
+    return send_msg_to_client(server, message(error));
 }
 
-void cmd_quit(server_s *server, char **args)
+void cmd_pass(server_t *server, char **args)
 {
-    send_msg_to_client(server, "221 Closing client\n");
+    enum ErrorKinds error = ALL_GOOD;
+    char *password = args[1];
+
+    user_t *user = server->users;
+    bool is_logged = (user->logged_in & server->client_socket) == server->client_socket;
+    bool is_awaiting_pass = (user->awaiting_pass & server->client_socket) == server->client_socket;
+    if (is_logged || !is_awaiting_pass)
+        return send_msg_to_client(server, message(NEED_ACCOUNT));
+    if (((strlen(user->password) == 0) && password == NULL) || (strcmp(user->password, password) == 0)) {
+        user->logged_in = server->client_socket;
+        user->awaiting_pass = 0;
+        return send_msg_to_client(server, "230 User logged in, proceed.");
+    }
+    if (password == NULL)
+        return send_msg_to_client(server, message(INVALID_PASSWORD));
+    if (strcmp(user->password, password) != 0)
+        return send_msg_to_client(server, message(INVALID_PASSWORD));
+    return send_msg_to_client(server, message(error));
 }
 
-void command_error(server_s *server, char *number)
+void cmd_quit(server_t *server, char **args)
 {
-    char *fmt = "%s Error (RFC compliant)\n";
-    dprintf(server->i, fmt, number);
+    return send_msg_to_client(server, "221 Service closing control connection.");
+}
+
+void cmd_pwd(server_t *server, char **args)
+{
+    //send_msg_to_client(server, getcwd(server->path, strlen(server->path)));
 }
