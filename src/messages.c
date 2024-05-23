10e5de3ef5ac8f3e236c773bd23584e58b074393
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-05-23 10:52:59 +0200

    [fix]: error messsages handling

--- src/messages.c
+++ src/messages.c
@@ -1,22 +1,19 @@
 #include <stdbool.h>
 #include "../include/server.h"
 
-#define INIT_MESSAGES()                                                                 \
-    messages[ALL_GOOD] = "??? This should be an error";                                   \
-    messages[INVALID_COMMAND] = "500 Syntax error, command unrecognized";                 \
-    messages[INVALID_PASSWORD] = "530 Authentication failed.";                            \
-    messages[INVALID_USERNAME] = "530 Authentication failed.";                            \
-    messages[INVALID_ARGUMENTS] = "501 Syntax error in parameters or arguments";          \
-    messages[USER_LOGGED] = "421 User already logged in, closing control connection";     \
-    messages[NEED_ACCOUNT] = "332 Need account for login.";
-
 const char *message(enum ErrorKinds kind)
 {
     static bool init = false;
     static char *messages[_KIND_COUNT];
 
     if (!init) {
-        INIT_MESSAGES();
+        messages[ALL_GOOD] = "??? This should be an error";
+        messages[INVALID_COMMAND] = "500 Syntax error, command unrecognized";
+        messages[INVALID_PASSWORD] = "530 Authentication failed.";
+        messages[INVALID_USERNAME] = "530 Authentication failed.";
+        messages[INVALID_ARGUMENTS] = "501 Syntax error in parameters or arguments";
+        messages[USER_LOGGED] = "421 User already logged in, closing control connection";
+        messages[NEED_ACCOUNT] = "332 Need account for login.";
         init = true;
     }
 
