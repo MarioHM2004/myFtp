44fd8f52844ce9bbeba84b92d6a862f53f11f5f9
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-05-23 11:25:03 +0200

    [fix]: client connection, removed unnecesary loop

--- src/client_connection.c
+++ src/client_connection.c
@@ -4,25 +4,26 @@
 ** File description:
 ** server socket
 */
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "../include/server.h"
-
+ 
 void send_msg_to_client(server_t *server, const char *msg)
 {
-    if (msg == NULL) {
+    int length = 0;
+ 
+    if (msg == NULL)
         return send_msg_to_client(server, "XXX null message :(");
-    }
-
-    int length = strlen(msg);
+    length = strlen(msg);
     write(server->client_socket, msg, length);
     write(server->client_socket, "\r\n", 2);
 }
-
+ 
 void client_commands(server_t *server)
 {
     cmd_t commands[] = {
@@ -39,22 +40,28 @@ void client_commands(server_t *server)
     }
     return send_msg_to_client(server, message(INVALID_COMMAND));
 }
-
-void client_connection(server_t *server)
+ 
+void client_connection(server_t *server, int client_socket)
 {
-    while (1) {
-        server->client_response = malloc(MAX_BYTES);
-        if (server->client_response == NULL) {
-            error("malloc failed");
-        }
-        if (read(server->client_socket, server->client_response, MAX_BYTES) == -1) {
+    int bytes = 0;
+    char *needle = "\r\n";
+    char *found = NULL;
+    server->client_socket = client_socket;
+    server->client_response = calloc(MAX_BYTES, 1);
+    if (server->client_response == NULL)
+        error("malloc failed");
+    bytes = read(server->client_socket, server->client_response, MAX_BYTES - 1);
+    if (bytes <= 0) {
+        if (bytes < 0)
             error("read failed");
-        }
-        char *needle = "\r\n";
-        char *found = strstr(server->client_response, needle);
-        if (found == NULL) {
+        close(server->client_socket);
+        FD_CLR(server->client_socket, &server->current_sockets);
+        free(server->client_response);
+        server->client_response = NULL;
+    } else {
+        found = strstr(server->client_response, needle);
+        if (found == NULL)
             error("clrf not found");
-        }
         *found = '\0';
         command_parsing(server);
     }
